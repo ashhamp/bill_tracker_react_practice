ba93fb58b2ee1f873b5acc4fe9a406ab49dc07d1
Ashley <ashhamp27@gmail.com>
2016-04-17 15:30:00 -0400

    fixes hound errors

--- spec/features/users/user_deletes_account_spec.rb
+++ spec/features/users/user_deletes_account_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-  # [X] As an authenticated user
-  # I want to delete my account
-  # So that my information is no longer retained by the app
+# [X] As an authenticated user
+# I want to delete my account
+# So that my information is no longer retained by the app
 
 feature "user deletes their account" do
   let!(:user1) { FactoryGirl.create(:user) }
@@ -12,6 +12,8 @@ feature "user deletes their account" do
     click_on("Update Account")
     click_on "Cancel my account"
 
-    expect(page).to have_content "You need to sign in or sign up before continuing"
+    expect(page).to have_content(
+      "You need to sign in or sign up before continuing"
+    )
   end
 end

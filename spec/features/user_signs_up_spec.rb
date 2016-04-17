b1482c1b6566aa661b247ca160fcd7f0aa1a3893
Ashley <ashhamp27@gmail.com>
2016-04-17 13:34:40 -0400

    adds test for authenticated user not seeing sign up

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 # [X] As a prospective user
 # I want to create an account
-# So that I can add bills
+# So that I can add bills and keep track of them
 
 feature "user authentication" do
+  let(:user1) { FactoryGirl.create(:user) }
 
   scenario "prospective user enters valid data to create account" do
     visit new_user_registration_path
@@ -30,5 +31,13 @@ feature "user authentication" do
     expect(page).to_not have_content "Welcome! You have signed up successfully."
     expect(page).to have_content "confirmation doesn't match Password"
     expect(page).to have_content "Email can't be blank"
+  end
+
+  scenario "an authenitcated user does not get redirected to the sign up page" do
+
+    sign_in(user1)
+    visit root_path
+    
+    expect(page).to_not have_content "Sign Up"
   end
 end

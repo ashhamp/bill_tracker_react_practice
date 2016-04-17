5491e113aba506cbf16727066b9053664cb2584f
Ashley <ashhamp27@gmail.com>
2016-04-16 21:29:34 -0400

    adds test for user sign in

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,15 @@
 FactoryGirl.define do
+  sequence :email do |n|
+    "usermcuser#{n}@example.com"
+  end
+
+  sequence :username do |n|
+    "usermcuser#{n}"
+  end
+
   factory :user do
+    username
+    email
+    password "iamalittlepassword"
   end
 end

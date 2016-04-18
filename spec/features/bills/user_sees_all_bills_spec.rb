51d987fcb554cf02ea71753ecaf3b73de912bbd5
Ashley <ashhamp27@gmail.com>
2016-04-18 14:57:37 -0400

    finished index for bills and some styling

--- spec/features/bills/user_sees_all_bills_spec.rb
+++ spec/features/bills/user_sees_all_bills_spec.rb
@@ -6,6 +6,9 @@ feature "authenticated user sees a list of their bills" do
   let!(:bill2) { FactoryGirl.create(:bill, user: user1) }
   let!(:bill3) { FactoryGirl.create(:bill, user: user1) }
 
+  let!(:user2) { FactoryGirl.create(:user) }
+  let!(:bill4) { FactoryGirl.create(:bill, user: user2) }
+
 
   scenario 'user signs in and sees a list of their bills' do
     sign_in(user1)
@@ -13,6 +16,6 @@ feature "authenticated user sees a list of their bills" do
     expect(page).to have_content(bill1.nickname)
     expect(page).to have_content(bill2.nickname)
     expect(page).to have_content(bill3.nickname)
-
+    expect(page).to_not have_content(bill4.nickname)
   end
 end
